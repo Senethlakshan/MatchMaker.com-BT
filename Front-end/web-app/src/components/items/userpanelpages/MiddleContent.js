baa3c36b525d6ef62876833a3fc1e986de589c2d
Senethlakshan <81217773+Senethlakshan@users.noreply.github.com>
2023-09-06 11:40:15 +0530

    update

--- Front-end/web-app/src/components/items/userpanelpages/MiddleContent.js
+++ Front-end/web-app/src/components/items/userpanelpages/MiddleContent.js
@@ -4,8 +4,8 @@ const MiddleContent = ({ toggleFavorites }) => {
   const [foodData, setFoodData] = useState([]);
 
   useEffect(() => {
-    // Fetch data from the API when the component mounts
-    fetch("http://localhost:8009/food-items")
+    
+    fetch("")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -25,7 +25,7 @@ const MiddleContent = ({ toggleFavorites }) => {
   return (
     <div className="bg-white rounded-xl p-4 md:w-1/2 mt-4 md:mt-0 md:overflow-y-auto">
       <h2 className="text-gray-800 text-xl font-semibold mb-2 bg-green-600">
-        Food Items
+        Feed
       </h2>
       <div className="bg-gray-400 p-1">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
